c3b7a73afd78efb7c9e526b9afee05ee1dba02ae
847311033 <847311033@qq.com>
2019-07-29 11:30:28 +0800

    mybatis cascding

--- ssm-base/src/main/java/com/king/ssm/mapper/chapter5/EmployeeMapper.java
+++ ssm-base/src/main/java/com/king/ssm/mapper/chapter5/EmployeeMapper.java
@@ -2,11 +2,9 @@ package com.king.ssm.mapper.chapter5;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
-
 import com.king.ssm.pojo.Employee;
 
-@Repository
+
 public interface EmployeeMapper {
 
 	public List<Employee> getEmployee(Long id);
---
+++ ssm-base/src/main/java/com/king/ssm/pojo/MaleEmployee.java
@@ -0,0 +1,15 @@
+package com.king.ssm.pojo;
+
+public class MaleEmployee extends Employee {
+
+	private MaleHealthForm maleHealthForm = null;
+
+	public MaleHealthForm getMaleHealthForm() {
+		return maleHealthForm;
+	}
+
+	public void setMaleHealthForm(MaleHealthForm maleHealthForm) {
+		this.maleHealthForm = maleHealthForm;
+	}
+
+}
