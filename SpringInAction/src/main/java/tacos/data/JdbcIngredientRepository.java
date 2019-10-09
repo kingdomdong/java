2013c77b53fc757a33e645f0e30843b37c407e75
847311033 <847311033@qq.com>
2019-10-09 22:49:46 +0800

    chapter 3     - h2 @Autowired

--- SpringInAction/src/main/java/tacos/data/JdbcIngredientRepository.java
+++ SpringInAction/src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -1,5 +1,6 @@
 package tacos.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import tacos.Ingredient;
@@ -12,6 +13,7 @@ public class JdbcIngredientRepository implements IngredientRepository {
 
     private JdbcTemplate jdbc;
 
+    @Autowired
     public JdbcIngredientRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
     }
