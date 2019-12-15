27924035868332fc5d652436f0a0d820caea932d
847311033 <847311033@qq.com>
2019-12-15 10:57:04 +0800

    Added static method Clock.toLocalTime().

--- kick-out-code-smell/src/main/java/clock/Clock.java
+++ kick-out-code-smell/src/main/java/clock/Clock.java
@@ -2,12 +2,13 @@ package clock;
 
 public abstract class Clock {
 
-    private final int UTC_OFFSET = 0;
+    private static final int UTC_OFFSET = 0;
     private int localTime = 0;
 
     public abstract void setLocalTime(int localTime);
 
-    public abstract int toLocalTime(int utcZeroTime);
-
+    public static int toLocalTime(int utcZeroTime) {
+        return utcZeroTime + UTC_OFFSET;
+    }
 
 }
