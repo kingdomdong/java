cdee62fabe3dd405da1acb6025bd1678ebd737d4
847311033 <847311033@qq.com>
2020-04-23 10:15:39 +0800

    add tddjava | cleancode

---
+++ javatdd/src/test/java/com/king/javatdd/JavatddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.javatdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
